7b175068ef476597dd22804e3c2a983871fd4574
市 幣 <whatnamecaniuseonjg99gle@gmail.com>
2026-09-17 05:17:09 +0000

    Link worksheet generator from the site footer.

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -15,6 +15,7 @@ const footerColumns = [
     links: [
       { href: "/resources", label: "西洋書法練習本" },
       { href: "/lab", label: "超级實驗室" },
+      { href: "/lab/generator", label: "字帖生成器" },
       { href: "/resources/italic-family-upper-straight", label: "斜體直筆家族" },
       { href: "/resources/copperplate-basic-strokes", label: "銅板基本筆畫" },
       { href: "/resources/spencerian-compound-curves", label: "斯賓塞複合曲線" },
